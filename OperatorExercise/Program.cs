c3f32ce8652eac87cdb9ccffe320557e8bc1995a
Toby Earle <toby_earle@yahoo.com>
2025-06-09 07:15:20 -0400

    completed operators exercise

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -2,9 +2,31 @@
 {
     public class Program
     {
+        public static double AreaOfACircle(double radius)
+        {
+            return Math.PI * Math.Pow(radius, 2);
+        }
+
+        public static void PrintAreaOfCircle()
+        {
+            Console.WriteLine("What is the radius of your circle?");
+            Double radius = Double.Parse(Console.ReadLine());
+            Console.WriteLine($"the area of a circle with a radius of {radius} is {AreaOfACircle(radius)}");
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int a = 17;
+            int b = 4;
+            
+            int sum = a + b;
+            int subtract = a - b;
+            int mult = a * b;
+            
+            int quotient = a / b;
+            int remainder = a % b;
+            
+            Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder} ");
+            PrintAreaOfCircle();
         }
     }
 }
